cb5bbff2bce5beec10f08db6f742db367d14ab4f
darshan45672 <drshnbhandary@gmail.com>
2024-03-26 08:10:10 +0530

    rendered component in homepage

--- app/Livewire/TodoList.php
+++ app/Livewire/TodoList.php
@@ -2,10 +2,25 @@
 
 namespace App\Livewire;
 
+use App\Models\Todo;
 use Livewire\Component;
 
 class TodoList extends Component
 {
+    // #[Rule('required|min:3| max:50')]
+    public $name ;
+    protected $rules = [
+        'name' => 'required|string|max:255|min:3',
+    ];
+    public function create(){
+        // $this->validate();
+
+        Todo::create([$this->validate()]);
+
+        $this->reset('name');
+
+        session()->flash('success', 'Todo Created Successfully');
+    }
     public function render()
     {
         return view('livewire.todo-list');
--- resources/views/livewire/todo-list.blade.php
+++ resources/views/livewire/todo-list.blade.php
@@ -1,33 +1,6 @@
 <div>
     {{-- The Master doesn't talk, he acts. --}}
-    <div class="container content py-6 mx-auto">
-        <div class="mx-auto">
-            <div id="create-form" class="hover:shadow p-6 bg-white border-blue-500 border-t-2">
-                <div class="flex ">
-                    <h2 class="font-semibold text-lg text-gray-800 mb-5">Create New Todo</h2>
-                </div>
-                <div>
-                    <form>
-                        <div class="mb-6">
-                            <label for="title"
-                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">*
-                                Todo </label>
-                            <input type="text" id="title" placeholder="Todo.."
-                                class="bg-gray-100  text-gray-900 text-sm rounded block w-full p-2.5">
-
-                            <span class="text-red-500 text-xs mt-3 block ">Error</span>
-
-                        </div>
-                        <button type="submit"
-                            class="px-4 py-2 bg-blue-500 text-white font-semibold rounded hover:bg-blue-600">Create
-                            +</button>
-                        <span class="text-green-500 text-xs">Saved.</span>
-
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
+    @include('livewire.includes.create-todo-box')
     <div id="search-box" class="flex flex-col items-center px-2 my-4 justify-center">
         <div class="flex justify-center items-center">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
