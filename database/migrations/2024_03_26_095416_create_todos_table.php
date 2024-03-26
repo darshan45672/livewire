071e8d4ed7d7616a4d5f8e6701f87307a2d7d38b
darshan45672 <drshnbhandary@gmail.com>
2024-03-26 15:26:32 +0530

    cretead a new migration

--- database/migrations/2024_03_26_095416_create_todos_table.php
+++ database/migrations/2024_03_26_095416_create_todos_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('todos', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->boolean('completed')->default(false);
             $table->timestamps();
         });
     }
